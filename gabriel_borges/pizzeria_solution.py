8108bd9470552eef6858b910c83627c526a562b2
Gabriel Borges <gabriel@ajouve.com>
2021-01-14 03:34:32 +0100

    Adding coments to the code

--- gabriel_borges/pizzeria_solution.py
+++ gabriel_borges/pizzeria_solution.py
@@ -1,22 +1,24 @@
-from __future__ import print_function
 import numpy as np
 import sys
-
-from numpy.core.fromnumeric import shape
-
-
-
 
 def solution(shape, nPizzerias, pizzerias ):
 
     #using numpy.zeros is easier than building an empy array by hand
     city = np.zeros(shape=(shape, shape))
 
+    #Going through every pizzeria
     for pizzeria in range(0, nPizzerias):
+        #Going through every column 
         for column in range(0, len(city)):
+            #Going through every row 
             for row in range(0, len(city[column])):
+                #Using the absolute value because the value can be negative
+                #Getting the difference between the cell y and the pizzeria y
                 diffY = abs(column - (pizzerias[pizzeria][0]-1))
+                #Getting the difference between the cell x and the pizzeria x
                 diffX = abs(row - (pizzerias[pizzeria][1]-1))
+                #DiffX + DiffY returns the manhattan distance between the cell and the pizzeria
+                #This value should be <= max distance that the delivery guy can travel in the manhattan distance
                 if (diffX + diffY <= pizzerias[pizzeria][2]):
                     city[column][row] += 1
 
@@ -37,4 +39,3 @@ if __name__ == "__main__":
         pizzerias.append([float(x) for x in fileData[i].split(' ')])
     
     print(solution(shape, nPizzerias, pizzerias))
-    
